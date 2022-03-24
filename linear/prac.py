69cb6dadaa001c6cc2b4bb03d0e634f636fc3c40
jihnkim <sysanal1697@gmail.com>
2022-03-24 11:53:20 +0900

    prac

--- linear/prac.py
+++ linear/prac.py
@@ -8,5 +8,22 @@ vec_col = np.array([
     [4],
 ])
 
-print(vec_row.shape)
-print(vec_col.shape)
+# print(vec_row.shape)
+# print(vec_col.shape)
+
+tmp = np.array([[2, 3, 4],
+[4, 6, 7],
+[8, 9, 0]])
+
+# print(tmp)
+
+#####
+import matplotlib.pyplot as plt
+
+p = lambda x: x**3 - x**2 - 1
+x = np.linspace(-100, 100, 100)
+
+plt.plot(x , p(x), color='blue', label='eq 1')
+plt.legend()
+
+plt.show()
